0e39f97c0a2d8affd9902b72d7eb5a114c495714
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2020-02-02 09:57:38 -0300

    Creating the app and its tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [httpState, setHttpState] = useState({
+    message: 'This is a static message'
+  });
+
+  useEffect(() => {
+
+    // You cannot return a Promise to useEffect
+    (async () => {
+      const joke = await getAJoke();
+
+      setHttpState({
+        message: joke
+      })
+    })();
+
+    // Optional. You can use a return to clean the side effect, like unsubscribing from events
+    return () => {}
+
+  }, [] /* This is important to not make a request twice (or more) */);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {httpState.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+async function getAJoke() {
+  const response = await axios.get('https://api.icndb.com/jokes/random');
+
+  if (response.data.type !== 'success') {
+    throw response;
+  }
+
+  return response.data.value.joke;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import nock from 'nock';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Mocking request', () => {
+  afterEach(() => {
+    cleanup();
+    nock.cleanAll();
+  })
+
+  it('It should render a message com Chuck Norris Pubic API', async () => {
+    nock('https://api.icndb.com')
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' }) // avoids CORS errors
+      .get('/jokes/random')
+      .delay(500)
+      .reply(200, {
+        "type": "success",
+        "value": {
+          "id": 323,
+          "joke": "Chuck Norris' programs never exit, they terminate.",
+          "categories": []
+        }
+      })
+
+    const { getByText } = render(<App />);
+
+    const [ staticMessageElement ] = await waitForElement(() => [
+      getByText('This is a static message')
+    ]); // It can wait for multiple elements
+
+    expect(staticMessageElement).toBeVisible();
+
+    const [ messageElement ] = await waitForElement(() => [
+      getByText("Chuck Norris' programs never exit, they terminate.")
+    ], {
+      timeout: 1000 // You can configure the timeout
+    });
+    
+    expect(messageElement).toBeVisible();
+  });
+})
